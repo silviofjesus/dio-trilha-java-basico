75677320188afd53a1823d66c6f8044fc8a4c659
silvio <silviofjesus@gmail.com>
2022-11-10 12:12:20 -0300

    atualizando

---
+++ Operadores.java/Operadores.java/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        
+        String nomeCompleto = "linguagem" + "java";
+
+        System.out.println(nomeCompleto);
+
+        System.out.println("nomecompleto");
+    }
+}
---
+++ Tipos-Variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    
+    
+    public static void main(String[] args) {
+
+        String nomeCompleto = "linguagem" + "java";
+
+        System.out.println(nomeCompleto);
+        
+    }
+}
